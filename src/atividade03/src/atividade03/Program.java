c3cac031fbdf72ef15da858b560d927e0f239d03
Italo Eduardo Carneiro da Silva <italo.carneiro@sga.pucminas.br>
2026-03-01 23:32:24 -0300

    feat: Lógica implementada da atividade 3 (Interseção entre vetores)

---
+++ src/atividade03/src/atividade03/Program.java
@@ -0,0 +1,42 @@
+package atividade03;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main(){
+		Scanner sc = new Scanner(System.in);
+		
+		int n, cont = 0;
+		
+		System.out.println("Digite o número de alunos para prosseguir com o processo de matricula:");
+		n = sc.nextInt();
+		
+		int[] matriculasPM = new int[n];
+		int[] matriculasMD = new int[n];
+		
+		System.out.println("PREENCHIMENTO DE MATRICULAS DOS ALUNOS DA DISCIPLINA DE PROGRAMACAO MODULAR\n");
+		for(int i=0; i < n; i++) {
+			System.out.printf("Digite a matricula do %dº aluno \n", i+1);
+			matriculasPM[i] = sc.nextInt();
+		}
+		
+		System.out.println("PREENCHIMENTO DE MATRICULAS DOS ALUNOS DAS DISCIPLINAS DE MODELAGEM DE PROCESSOS DE NEGOCIOS\n");
+		for(int i=0; i < n; i++) {
+			System.out.printf("Digite a matricula do %dº aluno:\n", i+1);
+			matriculasMD[i] = sc.nextInt();
+		}
+		
+		System.out.println("MATRICULA DOS ALUNOS MATRICULADOS EM AMBAS UCs:");
+		for(int i=0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if (matriculasPM[i] == matriculasMD[j]) {
+					System.out.printf("%d - %d \n", i+1, matriculasPM[i]);
+					cont++;
+				}
+			}
+		}
+		if (cont == 0) {
+			System.out.println("Não existe nenhum aluno matriculado em ambas UCs");
+		}
+	}
+}
